b454fa3d0086fbe1401ae38c8326be4601b675f4
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2021-10-13 13:32:59 +0200

    fix: add target for scully support of nx version 2

--- scully.ng-one-hundred.config.ts
+++ scully.ng-one-hundred.config.ts
@@ -5,6 +5,7 @@ export const config: ScullyConfig = {
   projectRoot: './apps/ng-one-hundred/src',
   projectName: 'ng-one-hundred',
   outDir: './dist/apps/ng-one-hundred-static',
+  target: 'targets',
   routes: {
     '/day/:slug': {
       type: NotionDomRouter,
