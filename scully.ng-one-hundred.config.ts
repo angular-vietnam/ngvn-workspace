4b9d0ada8b9316de698729f1b015fb1bf8a427dd
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2021-10-12 22:43:02 -0500

    feat: setup notion plugin

--- scully.ng-one-hundred.config.ts
+++ scully.ng-one-hundred.config.ts
@@ -1,8 +1,16 @@
+import { NotionDom, NotionDomRouter } from '@notion-stuff/scully-plugin-notion';
 import { ScullyConfig } from '@scullyio/scully';
 
 export const config: ScullyConfig = {
   projectRoot: './apps/ng-one-hundred/src',
   projectName: 'ng-one-hundred',
   outDir: './dist/apps/ng-one-hundred-static',
-  routes: {},
+  routes: {
+    '/day/:slug': {
+      type: NotionDomRouter,
+      postRenderers: [NotionDom],
+      databaseId: '0bad59ebe1394e03885fdf79e235d5bc',
+      basePath: '/day',
+    },
+  },
 };
