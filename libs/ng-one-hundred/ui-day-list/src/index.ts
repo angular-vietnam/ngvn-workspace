b8fb006a60b7fcefda9b1eb1ba4a5cc8ef43864a
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2021-10-15 16:07:53 -0500

    fix: export ui components

--- libs/ng-one-hundred/ui-day-list/src/index.ts
+++ libs/ng-one-hundred/ui-day-list/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/ng-one-hundred-ui-day-list.module';
+export * from './lib/day-list.component';
--- libs/ng-one-hundred/ui-home-banner/src/index.ts
+++ libs/ng-one-hundred/ui-home-banner/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/ng-one-hundred-ui-home-banner.module';
+export * from './lib/home-banner.component';
